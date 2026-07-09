6b8a0d4819fb64a439417b55776b3627cc48f472
Natali Schers <schersnatali@gmail.com>
2026-07-08 21:35:16 -0300

    feat: add GroupsController and DTOs for group management

---
+++ Divider/Divider/DTOs/CreateGroupDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Divider.DTOs;
+
+public class CreateGroupDto
+{
+    public required string Name { get; set; }
+    public required List<string> MemberNames { get; set; }
+}
---
+++ Divider/Divider/DTOs/MemberDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Divider.DTOs;
+
+public class MemberDto
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+}
