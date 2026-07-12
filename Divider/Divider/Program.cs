70bcf99ca22e0338eaf578bd3433a86d0c44c4e3
Natali Schers <schersnatali@gmail.com>
2026-07-12 15:23:19 -0300

    feat: enable Swagger and OpenAPI in all environments

--- Divider/Divider/Program.cs
+++ Divider/Divider/Program.cs
@@ -35,13 +35,9 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
